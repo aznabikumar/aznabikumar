44c7e0dceeea3b696a66bd49533a04b0a71146e0
aznabikumar <49429711+aznabikumar@users.noreply.github.com>
2021-12-01 19:11:17 +0530

    added java files

---
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -0,0 +1,12 @@
+package com.yourorganization.maven_sample;
+
+
+
+/**
+ * Some code that uses JavaParser.
+ */
+public class LogicPositivizer {
+    public static void main(String[] args) {
+        System.out.println("Hello World");  
+    }
+}
