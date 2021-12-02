37d3456af7b802e1694478661d12f33cdd6473db
aznabikumar <49429711+aznabikumar@users.noreply.github.com>
2021-12-02 17:27:53 +0530

    Update LogicPositivizer.java

--- src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
+++ src/main/java/com/yourorganization/maven_sample/LogicPositivizer.java
@@ -1,12 +1,14 @@
 package com.yourorganization.maven_sample;
 
-
+import com.github.javaparser.utils.Log;
 
 /**
  * Some code that uses JavaParser.
  */
 public class LogicPositivizer {
     public static void main(String[] args) {
-        System.out.println("Hello World");  
+        Log.setAdapter(new Log.StandardOutStandardErrorAdapter());
+        System.out.println("Hello World"); 
+        Log.info("Hello World! First Project");
     }
 }
